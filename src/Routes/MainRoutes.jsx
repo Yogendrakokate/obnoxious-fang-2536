e434a65aff219945d9f0c4cac2eac2fca3ab68ac
jeevan aloor <jeevanaloor500@gmail.com>
2022-12-15 22:43:47 +0530

    add day3

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -4,6 +4,8 @@ import HomePage from "../Pages/HomePage"
 import AddProject from "../Pages/Projects/AddProject";
 import Login from "../Pages/jeevan/Login"
 import Signup from "../Pages/jeevan/Signup"
+import Information from "../Pages/jeevan/Information";
+import Usefor from "../Pages/jeevan/Usefor";
 
 
 
@@ -15,6 +17,8 @@ const MainRoutes = () => {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/Signup" element={<Signup />}></Route>
         <Route path="/AddProject" element={<AddProject />}></Route>
+        <Route path="/Information" element={<Information />}></Route>
+        <Route path="/Usefor" element={<Usefor />}></Route>
       </Routes>
     </div>
   )
