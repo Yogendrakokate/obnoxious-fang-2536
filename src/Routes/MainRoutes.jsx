ba443204a0ab0486e6daf5a37d60915eea04d80e
Yogendra Kokate <101565730+Yogendrakokate@users.noreply.github.com>
2022-12-16 23:52:07 +0530

    day44

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -4,11 +4,10 @@ import HomePage from "../Pages/HomePage"
 import AddProject from "../Pages/Projects/AddProject";
 import Login from "../Pages/jeevan/Login"
 import Signup from "../Pages/jeevan/Signup"
-import Employe from "../Pages/EmployeePage";
-import SingleMember from "../Pages/Singlemem";
-import Desktime from "../Pages/jeevan/Desktime";
-import Usefor from "../Pages/jeevan/Usefor"
 import Information from "../Pages/jeevan/Information"
+import Usefor from "../Pages/jeevan/Usefor"
+import Desktime from "../Pages/jeevan/Desktime"
+
 
 const MainRoutes = () => {
   return (
@@ -17,8 +16,6 @@ const MainRoutes = () => {
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/Signup" element={<Signup />}></Route>
-        <Route path="/employe" element={<Employe />}></Route>
-        <Route path="/employe/:id" element={<SingleMember />}></Route>
         <Route path="/AddProject" element={<AddProject />}></Route>
         <Route path="/Information" element={<Information />}></Route>
         <Route path="/Usefor" element={<Usefor />}></Route>
